5b3205df5bf6359da75f3fd5285f9c8e1a8ae343
Kaung Myat Han <arkarhtethan@gmail.com>
2021-12-28 23:54:25 +0630

    phone Number file added

--- src/features/restaurant/components/RestaurantDetailsHeader.tsx
+++ src/features/restaurant/components/RestaurantDetailsHeader.tsx
@@ -1,12 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { faMapMarkerAlt, faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
 
 interface IRestaurantDetailsHeaderProps {
     name: string;
     imageUrl: string;
     location: string;
+    phone: string;
 }
-const RestaurantDetailsHeader = ({ name, location, imageUrl }: IRestaurantDetailsHeaderProps) => {
+const RestaurantDetailsHeader = ({ name, location, imageUrl, phone }: IRestaurantDetailsHeaderProps) => {
     return (
         <div
             className="h-full bg-cover bg-no-repeat w-full bg-center flex items-center relative"
@@ -16,14 +17,17 @@ const RestaurantDetailsHeader = ({ name, location, imageUrl }: IRestaurantDetail
             }}
         >
             <div className="bg-black h-full w-full absolute opacity-50"></div>
-            <div className="bg-white w-3/12 py-3 pl-16 z-10">
+            <div className="bg-white lg:w-3/12 py-3 pl-16 z-10 lg:pr-0 pr-12">
                 <h4 className="text-2xl font-semibold mb-2">
                     {name}
                 </h4>
                 <div className="flex space-x-3 items-center">
                     <FontAwesomeIcon icon={faMapMarkerAlt} />
                     <h6 className="text-base mt-1">{location}</h6>
-
+                </div>
+                <div className="flex space-x-3 items-center mt-2">
+                    <FontAwesomeIcon icon={faPhoneAlt} />
+                    <h6 className="text-base text-gray-800 font-semibold mt-1">{phone}</h6>
                 </div>
             </div>
 
