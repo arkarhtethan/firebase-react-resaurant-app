cf6ac3557faf1146cbd6fe69eb01af44b6c9bffc
Kaung Myat Han <arkarhtethan@gmail.com>
2021-12-28 23:54:45 +0630

    restaurant item responsiveness added

--- src/features/restaurant/components/RestaurantItem.tsx
+++ src/features/restaurant/components/RestaurantItem.tsx
@@ -1,14 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
-
+import { Link } from "react-router-dom"
 const RestaurantItem = () => {
     return (
-        <div className="w-1/4 my-3 px-1">
+        <Link to="/restaurant/1" className="lg:w-1/4 md:w-2/4 w-full my-3 px-1 sm:mx-auto">
             <div className="bg-white shadow-xl rounded-lg overflow-hidden">
                 <div>
                     <img
                         src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.iloveqatar.net%2Fpublic%2Fimages%2Flocal%2Fopen-restaurent-2.jpeg&f=1&nofb=1"
-                        className="bg-cover h-44" />
+                        className="bg-cover h-44 w-full" />
                 </div>
                 <div className="px-2 py-4">
                     <p className="font-bold text-lg">My Restaurant</p>
@@ -17,7 +17,7 @@ const RestaurantItem = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
