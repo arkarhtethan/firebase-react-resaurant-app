1a376bd3b9446b2d0789d8a298e180308ddd62cd
Kaung Myat Han <arkarhtethan@gmail.com>
2021-12-29 18:27:30 +0630

    firebase config file added

---
+++ src/utils/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDRNk9RoeDJ3HdxOVIsRgTYmnO1zrlfJVk",
+    authDomain: "restaurant-app-ad505.firebaseapp.com",
+    projectId: "restaurant-app-ad505",
+    storageBucket: "restaurant-app-ad505.appspot.com",
+    messagingSenderId: "923880518320",
+    appId: "1:923880518320:web:bae74fe9108a3f0318c175",
+    measurementId: "G-JRSVE4FR2Q",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+    return signInWithPopup(auth, provider)
+};
