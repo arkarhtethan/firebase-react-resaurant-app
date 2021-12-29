8b14793256280800566657691a17dfd05fa6051f
Kaung Myat Han <arkarhtethan@gmail.com>
2021-12-29 22:55:50 +0630

    userInfo added

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -6,14 +6,17 @@ import {
     User,
 } from "@firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
+import { getUser, IUser } from "../services/user";
 import { auth } from "../utils/firebase";
 interface IAuthContextType {
+    userInfo: IUser | null;
     currentUser: User | null;
     registerUser: null | ((email: string, password: string) => Promise<any>);
     loginUser: null | ((email: string, password: string) => Promise<any>);
     logoutUser: null | (() => Promise<any>);
 }
 const AuthContext = createContext<IAuthContextType>({
+    userInfo: null,
     currentUser: null,
     registerUser: null,
     loginUser: null,
@@ -24,10 +27,18 @@ export const useAuth = () => useContext(AuthContext);
 
 export default function AuthContextProvider ({ children }: { children: any }) {
     const [currentUser, setCurrentUser] = useState<User | null>(null);
+    const [userInfo, setUserInfo] = useState<IUser | null>(null);
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             setCurrentUser(user);
+            if (user) {
+                getUser(user.uid).then(querySnapshot => {
+                    querySnapshot.forEach((doc) => {
+                        setUserInfo(doc.data() as IUser);
+                    });
+                })
+            }
         });
         return () => {
             unsubscribe();
@@ -51,6 +62,7 @@ export default function AuthContextProvider ({ children }: { children: any }) {
         registerUser,
         loginUser,
         logoutUser,
+        userInfo,
     };
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
