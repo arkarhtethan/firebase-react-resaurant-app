1595fc24ed4dbe89421f74e3fd1a23e74e4b6f16
Kaung Myat Han <arkarhtethan@gmail.com>
2021-12-29 17:52:30 +0630

    disable button when invalid

--- src/shared/button/SolidButton.tsx
+++ src/shared/button/SolidButton.tsx
@@ -3,11 +3,12 @@ export interface ISolidButtonProps {
     text: string;
     buttonType?: "button" | "submit" | "reset" | undefined;
     classes?: string;
+    disabled?: boolean;
 }
 
-export default function SolidButton ({ onClick, text, buttonType = "button", classes }: ISolidButtonProps) {
+export default function SolidButton ({ onClick, text, buttonType = "button", classes, disabled = false }: ISolidButtonProps) {
     return (
-        <button type={buttonType} onClick={onClick} className={`${classes} bg-black text-white py-2 `}>
+        <button type={buttonType} onClick={onClick} className={`${classes} bg-black text-white py-2 `} disabled={disabled}>
             {text}
         </button>
     )
