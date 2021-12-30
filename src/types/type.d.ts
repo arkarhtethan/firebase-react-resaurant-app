997fafae0c9ffdc768f4f74773ff3513e967a826
Kaung Myat Han <arkarhtethan@gmail.com>
2021-12-30 17:38:45 +0630

    firebase type delcaration added

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -1,6 +1,7 @@
 declare module "react-datepicker";
 declare module "react-hook-form";
 declare module "react-js-pagination";
+declare module "firebase";
 declare module "react-loader-spinner";
 declare module "@fortawesome/react-fontawesome";
 declare module "@fortawesome/fontawesome-common-types";
