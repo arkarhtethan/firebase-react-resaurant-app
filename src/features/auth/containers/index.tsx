e21870de75643c67e91a3e4d946fa2c7482bf1fc
Kaung Myat Han <arkarhtethan@gmail.com>
2021-12-30 17:37:26 +0630

    navigate to previous path logic added

--- src/features/auth/containers/index.tsx
+++ src/features/auth/containers/index.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes, useLocation } from "react-router";
+import { useAuth } from "../../../context/AuthContext";
 import { Login } from "./Login";
 import { Register } from "./Register";
 
@@ -6,6 +7,16 @@ export { Login } from "./Login";
 export { Register } from "./Register";
 
 export default function Auth () {
+    const { currentUser } = useAuth();
+    const { state } = useLocation();
+    if (currentUser) {
+        const locationState = state as any;
+        if (locationState && locationState.redirectUrl) {
+            return <Navigate to={locationState.redirectUrl} />
+        } else {
+            return <Navigate to="/" />
+        }
+    }
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
