0793f64411bb2ed5627e49be0544409d59c390fc
Kaung Myat Han <arkarhtethan@gmail.com>
2021-12-30 17:59:38 +0630

    replace with env data

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDRNk9RoeDJ3HdxOVIsRgTYmnO1zrlfJVk",
-    authDomain: "restaurant-app-ad505.firebaseapp.com",
-    projectId: "restaurant-app-ad505",
-    storageBucket: "restaurant-app-ad505.appspot.com",
-    messagingSenderId: "923880518320",
-    appId: "1:923880518320:web:bae74fe9108a3f0318c175",
-    measurementId: "G-JRSVE4FR2Q",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
