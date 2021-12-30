c802b3c3481cbb1a8ce8f84c8e9610793f420c7a
Kaung Myat Han <arkarhtethan@gmail.com>
2021-12-30 17:42:49 +0630

    account delete page added

--- src/features/profile/container/AccountDelete.tsx
+++ src/features/profile/container/AccountDelete.tsx
@@ -1,11 +1,9 @@
 import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 import { SolidButton } from "../../../shared/button";
 import Header from "../../../shared/Header";
 import Modal from "../../../shared/Modal";
-import { logout } from "../../auth/authSlice";
 
 export default function AccountDelete () {
 
@@ -14,34 +12,23 @@ export default function AccountDelete () {
     }
 
     const navigate = useNavigate();
-    const dispatch = useDispatch();
-
-    // const [mutate] = useMutation(DELETE_ACCOUNT_MUTATION, {
-    //     onCompleted: ({ deleteAccount }) => {
-    //         if (deleteAccount.ok) {
-    //             dispatch(logout())
-    //             navigate('/')
-    //         }
-    //     },
-    // });
 
     const deleteAccount = () => {
-        // mutate();
         setShowModal(false);
     }
 
     const [showModal, setShowModal] = useState(false);
 
     return (
-        <div id="accountPanel" className="px-10 py-5 text-gray-900">
+        <div id="accountPanel" className="lg:px-10 px-4 py-5 text-gray-900">
             <Header title="Account" description="Your account details." />
-            <h3 className="text-2xl mb-4 font-bold">Account Settings</h3>
+            <h3 className="lg:text-2xl text-lg mb-4 font-bold">Account Settings</h3>
             <hr className="border-black" />
-            <div className="mt-8">
-                <h5 className="mb-3 text-xl">
+            <div className="lg:mt-8 mt-3">
+                <h5 className="lg:mb-3 mb-2 lg:text-xl text-lg">
                     Delete your account
                 </h5>
-                <p className="mb-3">Once you delete your account, there is no going back.</p>
+                <p className="mb-3 text-sm">Once you delete your account, there is no going back.</p>
                 <SolidButton text="Delete" onClick={confirmDelete} classes="px-10 focus:outline-none" />
             </div>
             <Modal
