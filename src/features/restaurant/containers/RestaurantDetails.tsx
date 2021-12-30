49f338e3f566e6e6b717becf25a4599ae0e2184d
Kaung Myat Han <arkarhtethan@gmail.com>
2021-12-30 17:39:18 +0630

    replace mock info with api data

--- src/features/restaurant/containers/RestaurantDetails.tsx
+++ src/features/restaurant/containers/RestaurantDetails.tsx
@@ -1,37 +1,163 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import RestaurantDetailsHeader from "../components/RestaurantDetailsHeader";
 import MenuItem from "../components/MenuItem";
+import { useNavigate, useParams } from "react-router";
+import { useCallback, useEffect, useState } from "react";
+import { getRetaurantById, IRestaurant } from "../../../services/restaurant";
+import { getMenuItems, IMenu } from "../../../services/menu";
+import LoadingCmp from "../../../shared/loader/LoadingCmp";
 
-const RestaurantDetails = () => {
-    let menuItems = Array.from(Array(10).keys());
+const RestaurantDetails = (props: any) => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [fetching, setFetching] = useState(false);
+    const [search, setSearch] = useState("");
+    const [searchResults, setSearchResults] = useState<IMenu[]>([]);
+    const [restaurant, setRestaurant] = useState<IRestaurant | null>();
+    const [menuItems, setMenuItems] = useState<IMenu[]>([]);
+
+    const fetchRestaurantDetails = useCallback(() => {
+        if (id) {
+            setFetching(true);
+            getRetaurantById(id).then((snapshot) => {
+                setFetching(false);
+                if (snapshot.exists()) {
+                    const restaurantData = {
+                        ...snapshot.data(),
+                        id: snapshot.id,
+                    } as IRestaurant;
+                    setRestaurant(restaurantData);
+                    fetchMenuByRestaurantId(restaurantData.id!);
+                } else {
+                    navigate("/404");
+                }
+            });
+        }
+    }, [id, navigate]);
+
+    const sortMenuItemByName = () => {
+        if (search.length > 0) {
+            const menuItemsToSort = searchResults.slice();
+            const sortedList = menuItemsToSort.sort((a, b) =>
+                a.name > b.name ? 1 : -1
+            );
+            setSearchResults(sortedList);
+        }
+        else {
+            const menuItemsToSort = menuItems.slice();
+            const sortedList = menuItemsToSort.sort((a, b) =>
+                a.name > b.name ? 1 : -1
+            );
+            setMenuItems(sortedList);
+        }
+    };
+
+    const sortMenuItemByPrice = () => {
+        if (search.length > 0) {
+            let menuItemsToSort = searchResults.slice();
+            const sortedList = menuItemsToSort.sort((a, b) =>
+                a.price > b.price ? 1 : -1
+            );
+            setSearchResults(sortedList);
+        } else {
+            let menuItemsToSort = menuItems.slice();
+            const sortedList = menuItemsToSort.sort((a, b) =>
+                a.price > b.price ? 1 : -1
+            );
+            setMenuItems(sortedList);
+        }
+    };
+
+    const fetchMenuByRestaurantId = (restaurantId: string) => {
+        getMenuItems(restaurantId).then((res) => {
+            const menuList: IMenu[] = [];
+            res.forEach((snapshot) => {
+                const menuData = { ...snapshot.data(), id: snapshot.id } as IMenu;
+                menuList.push(menuData);
+            });
+            const sortedList = menuList.sort((a, b) => (a.name > b.name ? 1 : -1));
+            setMenuItems(sortedList);
+            setFetching(false);
+        });
+    };
+
+    const sortTypeChangeHandler = (event: any) => {
+        const sortBy = event.target.value;
+        if (sortBy === "price") {
+            sortMenuItemByPrice();
+        } else {
+            sortMenuItemByName();
+        }
+    };
+
+    const searchChanged = (event: any) => {
+        const searchTerm = event.target.value;
+        setSearch(searchTerm);
+        if (searchTerm.length > 0) {
+            const menuList = [...menuItems];
+            const results = menuList.filter((item) =>
+                item.name.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+            setSearchResults(results);
+        }
+    };
+
+    useEffect(() => {
+        if (id) {
+            fetchRestaurantDetails();
+        }
+    }, [id, fetchRestaurantDetails]);
+
+    if (fetching || !restaurant) {
+        return (
+            <div className="h-screen flex justify-center items-center">
+                <LoadingCmp />
+            </div>
+        );
+    }
+
     return (
         <div className="">
             <div className="h-80 w-full">
                 <RestaurantDetailsHeader
-                    name="Karaweik Cafe"
-                    location="Mandalay"
-                    phone="+9509444054610"
-                    imageUrl="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.iloveqatar.net%2Fpublic%2Fimages%2Flocal%2Fopen-restaurent-2.jpeg&f=1&nofb=1"
+                    name={restaurant.name}
+                    city={restaurant.city}
+                    street={restaurant.street}
+                    phone={restaurant.phoneNumber}
+                    imageUrl={restaurant.image}
                 />
             </div>
             <div className="p-5 shadow-lg h-full bg-white py-12">
                 <h1 className="mb-2 font-bold text-xl text-gray-800">Menu Items</h1>
                 <hr className="border-gray-500" />
-                <div className="my-12 ml-2 flex space-x-12">
+                <div className="my-12 ml-2 flex lg:flex-row flex-col lg:space-x-12 space-y-12 lg:space-y-0">
                     <div className="flex space-x-4 items-center">
                         <p>Sort By</p>
-                        <select className="w-24">
-                            <option>Name</option>
-                            <option>Price</option>
+                        <select className="w-24" onChange={sortTypeChangeHandler}>
+                            <option value="name">Name</option>
+                            <option value="price">Price</option>
                         </select>
                     </div>
                     <div>
-                        <input placeholder="Search" className="border-b-2 border-gray-600 outline-none" />
+                        <input
+                            onChange={searchChanged}
+                            placeholder="Search"
+                            className="border-b-2 border-gray-600 outline-none"
+                        />
                     </div>
                 </div>
-                <div className="flex flex-wrap mt-5">
-                    {menuItems.map(menuItem => <MenuItem name="Burger" price={20} imageUrl="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpapersdsc.net%2Fwp-content%2Fuploads%2F2016%2F09%2FJunk-Food-Pictures.jpg&f=1&nofb=1" />)}
+                <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1">
+                    {search.length > 0
+                        ?
+                        (
+                            searchResults.map((menuItem) => (
+                                <MenuItem key={menuItem.id} menu={menuItem} />
+                            ))
+                        )
+
+                        : menuItems.map((menuItem) => (
+                            <MenuItem key={menuItem.id} menu={menuItem} />
+                        ))}
                 </div>
             </div>
         </div>
