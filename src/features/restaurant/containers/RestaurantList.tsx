59167480fdab3c5979cfe1fd4da002656c8096d1
Kaung Myat Han <arkarhtethan@gmail.com>
2021-12-30 17:39:01 +0630

    replace fake data with api data

--- src/features/restaurant/containers/RestaurantList.tsx
+++ src/features/restaurant/containers/RestaurantList.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
+import { getRetaurants, IRestaurant } from "../../../services/restaurant";
+import LoadingCmp from "../../../shared/loader/LoadingCmp";
 import RestaurantItem from "../components/RestaurantItem";
 
 const RestaurantList = () => {
-    let restaurants = Array.from(Array(10).keys());
+    const [restaurants, setrestaurants] = useState<IRestaurant[]>([])
+    const [fetching, setFetching] = useState(false);
+
+    const fetchRestaurants = () => {
+        setFetching(true);
+        getRetaurants().then((res) => {
+            const restaurantList: IRestaurant[] = []
+            res.forEach(snapshot => {
+                const restaurant: IRestaurant = { ...snapshot.data(), id: snapshot.id } as IRestaurant
+                restaurantList.push(restaurant);
+            })
+            setFetching(false);
+            setrestaurants(restaurantList);
+        }).catch((err) => {
+            setFetching(false);
+        })
+    }
+
+    useEffect(() => {
+        fetchRestaurants();
+    }, [])
+
+    if (fetching) {
+        return <div className="flex items-center justify-center min-h-screen">
+            <LoadingCmp />
+        </div>
+    }
+
     return (
-        <div className="w-4/5 mx-auto my-5 flex flex-wrap">
-            {restaurants.map(restaurant => <RestaurantItem />)}
+        <div className={`w-4/5 md:mx-auto  my-5 flex flex-wrap`}>
+            {restaurants.map((restaurant, index) => <RestaurantItem key={index} restaurant={restaurant} />)}
         </div>
     )
 }
