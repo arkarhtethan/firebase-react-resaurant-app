36024d54a35dab17b169286c076b8cbd5584bd9a
Kaung Myat Han <arkarhtethan@gmail.com>
2021-12-30 17:41:34 +0630

    routes added and wrapped with authcontext provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import { Router, Routes, Route } from "react-router-dom";
-import { Navbar } from "./components";
+import { Routes, Route } from "react-router-dom";
+import { Footer, Navbar } from "./components";
 import Auth from "./features/auth/containers";
-import Home from "./features/home/container";
+import RestaurantList from "./features/restaurant/containers/RestaurantList";
+import Restaurant from "./features/restaurant/containers";
+import Profile from "./features/profile/container";
+import AuthContextProvider from "./context/AuthContext";
+import PageNotFound from "./components/PageNotFound";
 
 
 export default function App () {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <div className="bg-gray-200 flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth/*" element={<Auth />} />
-        </Routes>
+    <AuthContextProvider>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="bg-gray-200 flex-grow">
+          <Routes>
+            <Route path="/" element={<RestaurantList />} />
+            <Route path="/auth/*" element={<Auth />} />
+            <Route path="/restaurant/*" element={<Restaurant />} />
+            <Route path="/profile/*" element={<Profile />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-    </div>
+    </AuthContextProvider>
   )
 }
